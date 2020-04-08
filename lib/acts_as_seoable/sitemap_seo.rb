ff5a88ab1953ef1ac9510604a225e255ea633e1e
Banura Randika <banura.r@fidenz.com>
2020-04-08 18:38:34 +0530

    KWRAV-566 #Issues fixed

--- lib/acts_as_seoable/sitemap_seo.rb
+++ lib/acts_as_seoable/sitemap_seo.rb
@@ -4,7 +4,7 @@ class SitemapSeo < ActiveRecord::Base
   validates_presence_of   :sitemap_action, :sitemap_controller
   validates_uniqueness_of :sitemap_action, scope: :sitemap_controller
   validates_numericality_of :priority, greater_than_or_equal_to: 0, less_than_or_equal_to: 1
-  
+
 
   enum period: { not_available: 0, always: 1, hourly: 2, daily: 3, weekly: 4, monthly: 5,yearly: 6, never: 7 }
 end
