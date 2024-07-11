43644a6f197f11671ed3cfab21a079e40e6ad950
Srimal.w <srimal.w@fidenz.com>
2024-07-11 20:30:40 +0700

    add site map link and remove debug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   sitemap_enable = false
   sitemap_enable = true if FiSeo.initialized_config.sitemap_enable == true
   if sitemap_enable
-    get 'sitemap' => 'sitemap#sitemap', format: true
+    get 'sitemap' => 'sitemap#sitemap', format: true, as: :sitemap
   end
 end
